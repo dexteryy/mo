ee8930b17b32fd864500cc4ffd36331812f80b78
dexteryy <dexter.yy@gmail.com>
2013-06-25 18:40:26 +0800

    mo/browsers: supports webview

--- browsers.js
+++ browsers.js
@@ -9,7 +9,7 @@
  */
 define("mo/browsers", [], function(){
 
-    var match, skin, os, is_mobile,
+    var match, skin, os, is_mobile, is_webview,
         ua = this.navigator.userAgent.toLowerCase(),
         rank = { 
             "360ee": 2,
@@ -30,6 +30,7 @@ define("mo/browsers", [], function(){
             randroid = /(android)[ ;]([\w.]*)/,
             rmobilesafari = /(\w+)[ \/]([\w.]+)[ \/]mobile.*safari/,
             rsafari = /(\w+)[ \/]([\w.]+) safari/,
+            rwebview = /(.)([^\/]+)[ \/]mobile\//,
             rwebkit = /(webkit)[ \/]([\w.]+)/,
             ropera = /(opera)(?:.*version)?[ \/]([\w.]+)/,
             rmsie = /(msie) ([\w.]+)/,
@@ -61,7 +62,7 @@ define("mo/browsers", [], function(){
             || ua.indexOf("compatible") < 0 && rmozilla.exec(ua) 
             || [];
 
-        is_mobile = rmobilesafari.exec(ua);
+        is_mobile = rmobilesafari.exec(ua) || (is_webview = rwebview.exec(ua));
 
         if (match[1] === 'webkit') {
             var vendor = is_mobile || rsafari.exec(ua);
@@ -75,8 +76,9 @@ define("mo/browsers", [], function(){
                         || os[1] === 'android' 
                             && 'aosp' 
                         || 'safari')
+                    || is_webview && 'webview'
                     || vendor[1];
-                match[2] = vendor[2];
+                match[2] = is_webview ? 0 : vendor[2];
             }
         }
 
